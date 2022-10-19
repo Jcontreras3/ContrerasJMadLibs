c12478dcd5a5fcd25b151021346f334aa6ebb764
Jovann Contreras <jcontreras@codestack.co>
2022-10-19 09:50:16 -0700

    Cleared console

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Jovann Contreras
+//Date 10/19/2022
+//Having multiple options for users to type in so that they can create a story.
